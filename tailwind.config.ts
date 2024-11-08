3c1b073a6b0fc8cc8a77357c142c27008cc1b30a
sadegh akbari <akbarisadegh382@gmail.com>
2024-11-08 13:48:51 +0330

    fix: theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Custom background and foreground colors
+        pink: '#ffeeea',
+        gray: '#f8fafc',
+        blueGray: '#f4fafc',
+        red: '#ee786b',
+        // These can be used as text or border colors
+        redText: '#ee786b',
+        blue: '#32a2bb',
+        grayText: '#aeb6bf',
+      },
+      backgroundColor: {
+        // Custom background colors (you can use these with bg-[color])
+        pink: '#ffeeea',
+        gray: '#f8fafc',
+        blueGray: '#f4fafc',
+        red: '#ee786b',
       },
     },
   },
